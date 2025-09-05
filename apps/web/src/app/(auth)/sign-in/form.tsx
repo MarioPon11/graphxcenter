bb0d4ec2f21da5e18dbb0fec07213aa91094a496
Mario Pon <mario.pon@graphxsource.hn>
2025-09-05 06:48:54 -0600

    fix: consistant button language

--- apps/web/src/app/(auth)/sign-in/form.tsx
+++ apps/web/src/app/(auth)/sign-in/form.tsx
@@ -104,7 +104,7 @@ export function LoginForm({
                 )}
               />
               <Button type="submit" className="w-full">
-                Login
+                Continue with email
               </Button>
             </div>
             <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
