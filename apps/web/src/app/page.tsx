2b38e9b8da16a72f3d7770d08b0fbbab585fce46
Mario Pon <mariobpon11@gmail.com>
2025-09-07 11:23:39 -0600

    feat: added admin layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@/server/auth";
+import { headers } from "next/headers";
+import { adminRoles } from "@/server/auth/access/admin";
 
-import { Button } from "@repo/ui/components/button";
-import { Logo } from "@/components/icons/logo";
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default function Home() {
-  return (
-    <main className="flex h-dvh w-dvw flex-col items-center justify-center">
-      <Logo />
-      <h1>Hello World</h1>
-      <Button asChild>
-        <Link href="/sign-in">Go to sign in</Link>
-      </Button>
-    </main>
-  );
+  if (session) {
+    if (adminRoles.includes(session.user.role ?? "")) {
+      redirect("/admin");
+    } else {
+      redirect("/dashboard");
+    }
+  } else {
+    redirect("/sign-in");
+  }
 }
---
+++ apps/web/src/components/admin-sidebar/admin-sidebar.tsx
@@ -0,0 +1,157 @@
+import React from "react";
+import {
+  BookOpen,
+  Bot,
+  Frame,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react";
+
+import { NavAdmin } from "@/components/admin-sidebar/nav-admin";
+import { NavMain } from "@/components/admin-sidebar/nav-main";
+import { NavUser } from "@/components/admin-sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@repo/ui/components/sidebar";
+import { APP_NAME } from "@/constants";
+import { Logo } from "@/components/icons";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navAdmin: [
+    {
+      title: "Playground",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Models",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Genesis",
+          url: "#",
+        },
+        {
+          title: "Explorer",
+          url: "#",
+        },
+        {
+          title: "Quantum",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Documentation",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Introduction",
+          url: "#",
+        },
+        {
+          title: "Get Started",
+          url: "#",
+        },
+        {
+          title: "Tutorials",
+          url: "#",
+        },
+        {
+          title: "Changelog",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navMain: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <div>
+          <Logo />
+          <span>{APP_NAME}</span>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavAdmin items={data.navAdmin} />
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
