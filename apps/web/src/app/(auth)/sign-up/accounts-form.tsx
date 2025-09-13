50ec604365e156f713be16da16644b5e65abbe0e
Mario Pon <mariobpon11@gmail.com>
2025-09-13 14:51:28 -0600

    fix: disabled next step before continue

--- apps/web/src/app/(auth)/sign-up/accounts-form.tsx
+++ apps/web/src/app/(auth)/sign-up/accounts-form.tsx
@@ -2,9 +2,12 @@
 
 import React from "react";
 import { AccountsCards } from "@/components/accounts-card";
+import { api } from "@/trpc/react";
 import { Button } from "@repo/ui/components/button";
 
 export function AccountsForm() {
+  const { data } = api.auth.accounts.list.useQuery();
+
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-bold">Accounts</h1>
@@ -13,7 +16,9 @@ export function AccountsForm() {
         <Button variant="outline" className="flex-1">
           Back
         </Button>
-        <Button className="flex-1">Next</Button>
+        <Button className="flex-1" disabled={(data?.length ?? 0) <= 1}>
+          Next
+        </Button>
       </div>
     </div>
   );
